769acf9bba086d869dc7a5c0a4b58d45a87a80db
rstyro <rstyro>
2025-10-29 10:04:32 +0800

    spring源码OllamaApiAutoConfiguration没有自动注入，这里补充一下

--- mcp/mcp-client-annotation/src/main/java/top/lrshuai/ai/mcp/client/annotation/McpClientAnnotationApplication.java
+++ mcp/mcp-client-annotation/src/main/java/top/lrshuai/ai/mcp/client/annotation/McpClientAnnotationApplication.java
@@ -2,17 +2,10 @@ package top.lrshuai.ai.mcp.client.annotation;
 
 import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.spec.McpSchema;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.Map;
@@ -22,21 +15,6 @@ public class McpClientAnnotationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(McpClientAnnotationApplication.class, args);
-    }
-
-    @Value("${spring.ai.ollama.base-url}")
-    private String baseUrl;
-
-    @Bean
-    @ConditionalOnMissingBean
-    public OllamaApi ollamaApi(ObjectProvider<RestClient.Builder> restClientBuilderProvider,
-                               ObjectProvider<WebClient.Builder> webClientBuilderProvider, ResponseErrorHandler responseErrorHandler) {
-        return OllamaApi.builder()
-                .baseUrl(baseUrl)
-                .restClientBuilder(restClientBuilderProvider.getIfAvailable(RestClient::builder))
-                .webClientBuilder(webClientBuilderProvider.getIfAvailable(WebClient::builder))
-                .responseErrorHandler(responseErrorHandler)
-                .build();
     }
 
     @Bean
@@ -56,7 +34,6 @@ public class McpClientAnnotationApplication {
                         .build();
                 McpSchema.CallToolResult response = mcpClient.callTool(toolRequest);
                 System.out.println("Tool response: " + response);
-
             }
         };
     }
